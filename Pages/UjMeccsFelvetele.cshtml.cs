bfe88bc8cc81e3a2def0242dd5e057869436992b
hunor64 <nhunor06@gmail.com>
2024-09-16 13:41:02 +0200

    Added form

--- Pages/UjMeccsFelvetele.cshtml.cs
+++ Pages/UjMeccsFelvetele.cshtml.cs
@@ -1,3 +1,4 @@
+using FociWeb_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,22 @@ namespace FociWeb_App.Pages
 {
     public class UjMeccsFelveteleModel : PageModel
     {
+        [BindProperty]
+        public Meccs UjMeccs { get; set; }
+        public List<Meccs> meccsekListaja { get; set; } = new();
+        private readonly FociDbContext _db;
+        public UjMeccsFelveteleModel(FociDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost() 
+        {
+            _db.Meccsek.Add(UjMeccs);
+            return Page();
         }
     }
 }
