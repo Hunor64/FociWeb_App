66c8b811fb1943fbace521e580340493eb999bf9
hunor64 <nhunor06@gmail.com>
2024-09-16 13:36:33 +0200

    Added DB

---
+++ Pages/UjMeccsFelvetele.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FociWeb_App.Pages
+{
+    public class UjMeccsFelveteleModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
