92027657369dabb518713f4d5257643ef97c408d
hunor64 <nhunor06@gmail.com>
2024-09-16 12:56:38 +0200

    Add project files.

---
+++ Models/Meccs.cs
@@ -0,0 +1,16 @@
+﻿namespace FociWeb_App.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg {  get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFel {  get; set; }
+        public int VendegFel { get; set; }
+
+        public string HazaiCsapat {  get; set; }
+        public string VendegCsapat { get; set; }
+
+    }
+}
