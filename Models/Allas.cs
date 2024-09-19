0c414cd609e3bdf28540790aebe380c87123ada7
hunor64 <nhunor06@gmail.com>
2024-09-19 09:17:39 +0200

    Óra vége

---
+++ Models/Allas.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace FociWeb_App.Models
+{
+    public class Allas
+    {
+        public int Helyezes{ get; set; } = 0;
+        public string Nev { get; set; }
+        public int Gyozelmek { get; set; }
+        public int Dontetlenek { get; set; }
+        public int Veresegek { get; set; }
+        public int LottGolok { get; set; }
+        public int KapottGolok { get; set; }
+        public int Pontok { get; set; } = 0;
+
+        public Allas(string nev, int gyozelmek, int dontetlenek, int veresegek, int lottGolok, int kapottGolok, int pontok)
+        {
+            this.Nev = nev;
+            this.Gyozelmek = gyozelmek;
+            this.Dontetlenek = dontetlenek;
+            this.Veresegek = veresegek;
+            this.LottGolok = lottGolok;
+            this.KapottGolok = kapottGolok;
+            this.Pontok = pontok;
+        }
+        public Allas(int helyezes)
+        {
+            this.Helyezes = helyezes;
+        }
+    }
+}
---
+++ Pages/Tabella.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FociWeb_App.Models;
+
+namespace FociWeb_App.Pages
+{
+    public class TabellaModel : PageModel
+    {
+        List<Allas> allasTabella;
+        private readonly FociWeb_App.Models.FociDbContext _context;
+
+        public TabellaModel(FociWeb_App.Models.FociDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Meccs> Meccs { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Meccs = await _context.Meccsek.ToListAsync();
+        }
+    }
+}
